e9a40dc650e7f38558d2919b155cb5a78f16c02c
paulpaul168 <github@paulhoeller.at>
2025-01-04 22:06:18 +0100

    Fix eslint error

--- src/pages/api/departures.ts
+++ src/pages/api/departures.ts
@@ -8,7 +8,8 @@ export default async function handler(
         const response = await fetch('https://www.wienerlinien.at/ogd_realtime/monitor?rbl=90');
         const data = await response.json();
         res.status(200).json(data);
-    } catch (error) {
+    } catch (error: unknown) {
+        console.error('Error fetching departures:', error);
         res.status(500).json({ error: 'Failed to fetch data' });
     }
 } 
